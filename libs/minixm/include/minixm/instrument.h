e52948f28d499a12d6fecbc471b6e9fee8ed1c00
Marco Foco <pan@spinningkids.org>
2024-03-13 20:43:48 +0100

    Moving InstrumentVibrato at Instrument level

--- libs/minixm/include/minixm/instrument.h
+++ libs/minixm/include/minixm/instrument.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <cassert>
+#include <numbers>
 
 #include "envelope.h"
 #include "sample.h"
@@ -16,12 +17,12 @@ struct Instrument final
 	XMInstrumentHeader			header;
 	XMInstrumentSampleHeader	instrument_sample_header;
 #ifdef FMUSIC_XM_VOLUMEENVELOPE_ACTIVE
-	EnvelopePoints				volume_envelope;
+    EnvelopePoints				volume_envelope;
 #endif
 #ifdef FMUSIC_XM_PANENVELOPE_ACTIVE
-	EnvelopePoints				pan_envelope;
+    EnvelopePoints				pan_envelope;
 #endif
-	Sample						sample[16];		// 16 samples per instrument
+    Sample						sample[16];		// 16 samples per instrument
 
 	[[nodiscard]] const Sample& getSample(XMNote note) const
 	{
@@ -30,4 +31,59 @@ struct Instrument final
 		assert(note_sample >= 0 && note_sample < 16);
 		return sample[note_sample];
 	}
+
+#ifdef FMUSIC_XM_INSTRUMENTVIBRATO_ACTIVE
+    int				instrument_vibrato_position;   			// instrument vibrato position
+    int				instrument_vibrato_sweep_position;		// instrument vibrato sweep position
+
+    [[nodiscard]] int getInstrumentVibratoDelta()
+    {
+        int delta = 0;
+
+        switch (instrument_sample_header.vibrato_type)
+        {
+        case XMInstrumentVibratoType::Sine:
+        {
+            delta = static_cast<int>(sinf(static_cast<float>(instrument_vibrato_position) * (std::numbers::pi_v<float> / 128.0f)) * 256.0f);
+            break;
+        }
+        case XMInstrumentVibratoType::Square:
+        {
+            delta = 256 - (instrument_vibrato_position & 128) * 4;
+            break;
+        }
+        case XMInstrumentVibratoType::InverseSawTooth:
+        {
+            delta = (instrument_vibrato_position & 128) * 4 - (instrument_vibrato_position + 1);
+            break;
+        }
+        case XMInstrumentVibratoType::SawTooth:
+        {
+            delta = instrument_vibrato_position + 1 - (instrument_vibrato_position & 128) * 4;
+            break;
+        }
+        }
+
+        delta *= instrument_sample_header.vibrato_depth;
+        if (instrument_sample_header.vibrato_sweep)
+        {
+            delta *= instrument_vibrato_sweep_position;
+            delta /= instrument_sample_header.vibrato_sweep;
+        }
+
+        instrument_vibrato_sweep_position = std::min(instrument_vibrato_sweep_position + 1, static_cast<int>(instrument_sample_header.vibrato_sweep));
+        instrument_vibrato_position += instrument_sample_header.vibrato_rate;
+
+        return delta / 128;
+
+    }
+#endif
+
+    void reset()
+    {
+#ifdef FMUSIC_XM_INSTRUMENTVIBRATO_ACTIVE
+        instrument_vibrato_sweep_position = 0;
+        instrument_vibrato_position = 0;
+#endif
+    }
 };
