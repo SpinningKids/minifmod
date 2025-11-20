36db4def3c835e00f06e8affbf5cbc5f15408cda
Pan of SpinningKids <pansk@users.noreply.github.com>
2025-11-20 22:08:50 +0100

    Rewritten mix loop

--- libs/minixm/src/mixer_channel.cpp
+++ libs/minixm/src/mixer_channel.cpp
@@ -13,53 +13,49 @@
 #include <minixm/mixer_channel.h>
 
 #include <cmath>
-#include <limits>
 
 void MixerChannel::mix(float* mixptr, uint32_t len, float filter_k)
 {
+    if (!sample_ptr)
+    {
+        return;
+    }
     uint32_t sample_index = 0;
+    const auto loop_start = static_cast<float>(sample_ptr->header.loop_start);
+    const auto loop_length = static_cast<float>(sample_ptr->header.loop_length);
+    auto loop_end = static_cast<float>(loop_start + loop_length);
+
+    // Fixes potential bug: previously mix_positionwe play the sample until the end, and we wrapped back into the loop
+    const auto loop_mode = (speed > 0 && mix_position > loop_end) ? XMLoopMode::Off : sample_ptr->header.loop_mode;
+    if (loop_mode == XMLoopMode::Off) {
+        loop_end = static_cast<float>(sample_ptr->header.length);
+    }
+    auto sample_target = (speed > 0) ? loop_end : loop_start;
 
     //==============================================================================================
     // LOOP THROUGH CHANNELS
     //==============================================================================================
-    while (sample_ptr && len > sample_index)
+    while (len > sample_index)
     {
-        const auto loop_end = static_cast<float>(sample_ptr->header.loop_start + sample_ptr->header.
-            loop_length);
-
-        float samples_to_mix; // This can occasionally be < 0
-        if (speed > 0)
-        {
-            samples_to_mix = loop_end - mix_position;
-            if (samples_to_mix <= 0)
-            {
-                samples_to_mix = static_cast<float>(sample_ptr->header.length) - mix_position;
-            }
-        }
-        else
-        {
-            samples_to_mix = mix_position - static_cast<float>(sample_ptr->header.loop_start);
-        }
-
         // Ensure that we don't try to mix a negative amount of samples
-        const auto samples_to_mix_target = static_cast<unsigned int>(std::max(
-            0.f, ceilf(samples_to_mix / fabsf(speed)))); // round up the division
+        const auto samples_to_mix_target = static_cast<uint32_t>(ceilf(std::max(
+            0.f, (sample_target - mix_position) / speed))); // round up the division
 
         // =========================================================================================
         // the following code sets up a mix counter. it sees what will happen first, will the output buffer
         // end be reached first or will the end of the sample be reached first?
         // whatever is smallest will be the mix_count.
-        const unsigned int mix_count = std::min(len - sample_index, samples_to_mix_target);
+        const auto mix_count = std::min(len - sample_index, samples_to_mix_target);
 
         //= SET UP VOLUME MULTIPLIERS ==================================================
 
-        for (unsigned int i = 0; i < mix_count; ++i)
+        for (uint32_t i = 0; i < mix_count; ++i)
         {
             const auto mixpos = static_cast<uint32_t>(mix_position);
             const float frac = mix_position - static_cast<float>(mixpos);
-            const auto samp1 = sample_ptr->buff[mixpos];
-            const float newsamp = static_cast<float>(sample_ptr->buff[mixpos + 1] - samp1) * frac +
-                static_cast<float>(samp1);
+            const auto samp0 = static_cast<float>(sample_ptr->buff[mixpos]);
+            const auto samp1 = static_cast<float>(sample_ptr->buff[mixpos + 1]);
+            const auto newsamp = (samp1 - samp0) * frac + samp0;
             mixptr[0 + (sample_index + i) * 2] += filtered_left_volume * newsamp;
             mixptr[1 + (sample_index + i) * 2] += filtered_right_volume * newsamp;
             filtered_left_volume += (left_volume - filtered_left_volume) * filter_k;
@@ -74,39 +70,28 @@ void MixerChannel::mix(float* mixptr, uint32_t len, float filter_k)
         //=============================================================================================
         if (mix_count == samples_to_mix_target)
         {
-            if (sample_ptr->header.loop_mode == XMLoopMode::Normal)
+            switch (loop_mode)
             {
+            case XMLoopMode::Normal:
                 do
                 {
-                    mix_position -= static_cast<float>(sample_ptr->header.loop_length);
+                    mix_position -= loop_length;
                 } while (mix_position >= loop_end);
-            }
-            else if (sample_ptr->header.loop_mode == XMLoopMode::Bidi)
-            {
+                break;
+            case XMLoopMode::Bidi:
                 do
                 {
-                    if (speed < 0.f)
-                    {
-                        //BidiBackwards
-                        mix_position = static_cast<float>(2 * sample_ptr->header.loop_start) -
-                            mix_position - 1;
-                        speed = -speed;
-                        if (mix_position < loop_end)
-                        {
-                            break;
-                        }
-                    }
-                    //BidiForward
-                    mix_position = 2 * loop_end - mix_position - 1;
+                    mix_position = 2 * sample_target - mix_position - 1;
                     speed = -speed;
-                } while (mix_position < static_cast<float>(sample_ptr->header.loop_start));
-            }
-            else
-            {
+                    sample_target = (speed > 0) ? loop_end : loop_start;
+                } while ((sample_target - mix_position) * speed < 0.f);
+                break;
+            case XMLoopMode::Off:
+            default:
                 mix_position = 0;
                 sample_ptr = nullptr;
+                return;
             }
         }
     }
 }
-
